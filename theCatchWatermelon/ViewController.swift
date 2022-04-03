174fde1c1358f106b6f2187d14326d53494a1cfe
Sinan Tanrıkut <sinantanrikut7@gmail.com>
2022-04-03 17:02:11 +0300

    Initial Commit

---
+++ theCatchWatermelon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  theCatchWatermelon
+//
+//  Created by Sinan Tanrıkut on 3.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
